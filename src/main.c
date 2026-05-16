7646b6dc595bb8d73866fbcf8537cdb4d8708c53
ProsperousTroll <ivekilled4dogs@gmail.com>
2026-05-15 20:49:18 -0700

    initial commit to win branch

--- src/main.c
+++ src/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-#include <raylib.h>
 #include <time.h>
 #include "../inc/particles.h"
+#include "../inc/raylib.h"
 
 ParticleSystem pSystem;
+bool DEBUG = false;
 
 int run(int w, int h, char* name, int fps);
 void init();
@@ -15,15 +16,29 @@ int main(){
 }
 
 void init(){
+	SetConfigFlags(FLAG_WINDOW_ALWAYS_RUN | FLAG_WINDOW_RESIZABLE);
+	SetWindowMonitor(0);
+
 	pSystem = initSystem(200);
 }
 void update(float dt){
+	Vector2 mousePos = GetMousePosition();
 	updateSystem(&pSystem, dt);
+
+	if(IsMouseButtonDown(1)){
+		SetWindowPosition(GetWindowPosition().x + mousePos.x - (float)WIN_WIDTH/2, GetWindowPosition().y + mousePos.y - (float)WIN_HEIGHT/2);
+	}
+
+	if(IsKeyPressed(KEY_F3) && !DEBUG){
+		DEBUG = true;
+	} else if(IsKeyPressed(KEY_F3)) DEBUG = false;
 }
 void draw(){
 	ClearBackground(BLACK);
 	drawSystem(&pSystem);
-	DrawFPS(10, 10);
+	if(DEBUG) { 
+		DrawFPS(10, 10); 
+	}
 }
 
 int run(int w, int h, char* name, int fps){
